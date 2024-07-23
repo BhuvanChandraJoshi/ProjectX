fdc6d965910609586edd4a78c0c01fad3b7b5d61
hharsh0 <harsh.v7681@gmail.com>
2024-07-23 12:12:25 +0530

    add jpa

---
+++ Server/src/main/java/com/projectx/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.projectx.repository;
+
+import com.projectx.models.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, String> {
+}
---
+++ Server/src/main/java/com/projectx/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.projectx.repository;
+
+import com.projectx.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
